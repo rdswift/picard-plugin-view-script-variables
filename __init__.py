63cf49570f6e17738f68ad41cf7811da98921e0f
Bob Swift <bswift@rsds.ca>
2025-12-15 18:06:37 -0700

    Change NAME to TITLE to accommodate revised api

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from picard.plugin3.api import (
     File,
     PluginApi,
     Track,
+    t_,
 )
 from picard.tags import preserved_tag_names
 
@@ -15,11 +16,7 @@ from .ui_variables_dialog import Ui_VariablesDialog
 
 
 class ViewVariables(BaseAction):
-    NAME = 'View script variables'
-
-    def __init__(self):
-        super().__init__(api=self.api)
-        self.setText(self.api.tr("action.name", "View script variables"))
+    TITLE = t_("action.title", "View script variables")
 
     def callback(self, objs):
         obj = objs[0]
